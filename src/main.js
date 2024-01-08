50961b1144d0c44097688973ec1564375f2e367a
Andreas-206 <puma_real@ukr.net>
2024-01-08 16:02:36 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-fetch('https://pixabay.com/api/?key=41701983-23ca5d5908e2c78927e8095f2&q=yellow+flower')
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '41701983-23ca5d5908e2c78927e8095f2';
+
+const getBaseUrl = () => {
+  const url = new URL(BASE_URL);
+  url.searchParams.append("key", API_KEY)
+
+  return url;
+}
+
+const url = getBaseUrl();
+url.searchParams.append("q", "wolf");
+
+fetch(url)
   .then(res => res.json())
-  .then(images => console.log(images))
+  .then(images => {
+    document.body.insertAdjacentHTML("beforeend", `
+    <img src="${images.hits[0].largeImageURL}"/>
+    `)
+  })
