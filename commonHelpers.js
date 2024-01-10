7165b484a1240d1307da61e4f236fd77bcea6edd
Andreas-206 <141054318+Andreas-206@users.noreply.github.com>
2024-01-10 18:23:26 +0000

    Deploying to gh-pages from @ Andreas-206/goit-js-hw-11@bab89ba11c3acddd2d95cc64c32834d569c3450a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,8 @@
 import{S as d,i as u}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const n of t)if(n.type==="childList")for(const a of n.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function e(t){const n={};return t.integrity&&(n.integrity=t.integrity),t.referrerpolicy&&(n.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?n.credentials="include":t.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function s(t){if(t.ep)return;t.ep=!0;const n=e(t);fetch(t.href,n)}})();const m="https://pixabay.com/api/",f="41701983-23ca5d5908e2c78927e8095f2",h=()=>{const o=new URL(m);return o.searchParams.append("key",f),o},g=o=>{const r=h();return r.searchParams.append("q",o),fetch(r).then(e=>{if(!e.ok)throw new Error(`HTTP error! Status: ${e.status}`);return e.json()}).then(e=>e.hits).catch(e=>{throw console.error("Error fetching images:",e),e})},p=o=>{const r=document.getElementById("gallery");r.innerHTML="";const e=new d(".gallery-item");o.forEach(({image:s})=>{r.insertAdjacentHTML("beforeend",`
     <li class="gallery-item">
-    <img src="${webformatURL}" data-source="${largeImageURL}" alt="${tags}"/>
+    <a href="${largeImageURL}"
+      <img src="${webformatURL}" alt="${tags}"/>
+    <a/>
     <p>Likes: ${likes}<p/>
     <p>view: ${view}<p/>
     <p>comments: ${comments}<p/>
